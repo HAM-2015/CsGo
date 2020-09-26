a465b110114717caff6b36c4dcf7063a12f1d267
HAM-2015 <591170887@qq.com>
2020-09-26 13:03:11 +0800

    no message

--- CsGo/Go/channel.cs
+++ CsGo/Go/channel.cs
@@ -3521,7 +3521,7 @@ namespace Go
             return generator.csp_timed_invoke(this, ms, msg, lostRes, lostMsg);
         }
 
-        public delay_csp<csp_invoke_wrap<R>> wrap_csp_timed_invoke(int ms, T msg, Action<R> lostRes = null)
+        public delay_csp<csp_invoke_wrap<R>> wrap_timed_invoke(int ms, T msg, Action<R> lostRes = null)
         {
             return generator.wrap_csp_timed_invoke(this, ms, msg, lostRes);
         }
